7b008da62c62a9c64ef4a9f26f5c1129e246e5af
Yash Agrawal <yashagrawal9319029562@gmail.com>
2023-03-12 07:39:49 -0700

    insertion completed

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,4 +1,32 @@
 #include<stdio.h>
 int main(){
-    printf("Hello world");
+    int n,j,key;
+    printf("Enter the size of the array -> ");
+    scanf("%d",&n);
+    printf("Enter the elements of the array -> ");
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    // performing sorting here 
+    for (int i = 1; i < n; i++)    {
+        key = arr[i];
+        j = i-1;
+        while (j>=0 && arr[j]>key)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+        
+    }
+    printf("Printing the sorted array -> ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    
 }
